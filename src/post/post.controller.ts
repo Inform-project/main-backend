36cee1cbb843b45af2cc1c2d65197e99cc608a27
sonchaegeon <thscorjs1002@naver.com>
2021-02-14 01:54:39 +0900

    UPDATE create post controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { CreatePostDto } from './entity/post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+    constructor(private postService: PostService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async createPost(@Body() body: CreatePostDto) {
+        await this.postService.createPost(body);
+        return "Post created";
+    }
+}
