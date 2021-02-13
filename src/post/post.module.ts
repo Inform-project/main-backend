49c15c871467c316185ff691faf6f91fdb3bfa80
sonchaegeon <thscorjs1002@naver.com>
2021-02-14 01:54:29 +0900

    UPDATE imports post repository, user repository

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from 'src/auth/entity/user.repository';
+import { PostRepository } from './entity/post.repository';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([PostRepository, UserRepository]),
+  ],
   controllers: [PostController],
   providers: [PostService]
 })
