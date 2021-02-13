d5d5ac9e83f230d7320600c9621f0a90769c899f
sonchaegeon <thscorjs1002@naver.com>
2021-02-14 08:02:54 +0900

    UPDATE update post service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,11 +1,11 @@
-import { Inject, Injectable, Scope } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable, NotFoundException, Scope } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Request } from 'express';
 import { User } from 'src/auth/entity/user.entity';
 import { UserRepository } from 'src/auth/entity/user.repository';
 import { TokenPayload } from 'src/auth/interface/payload.interface';
-import { CreatePostDto } from './entity/post.dto';
+import { CreatePostDto, UpdatePostDto } from './entity/post.dto';
 import { Post } from './entity/post.entity';
 import { PostRepository } from './entity/post.repository';
 
@@ -18,9 +18,22 @@ export class PostService {
     ) {}
 
     public async createPost(body: CreatePostDto): Promise<void> {
+        await this.postRepository.createPost(body, await this.getUser());
+    }
+
+    public async updatePost(body: UpdatePostDto): Promise<void> {
+        if(!await this.postRepository.findOne(body.id)){
+            throw new NotFoundException;
+        }
+        if(!((await this.getUser()).id === (await this.postRepository.findUserByPostId(body.id)))){
+            throw new ForbiddenException;
+        }
+        await this.postRepository.updatePost(body);
+    }
+
+    private async getUser(): Promise<User> {
         const payload = this.request.user as TokenPayload;
-        const user: User = await this.userRepository.findOne(payload.id);
-        
-        await this.postRepository.createPost(body, user);
+        const user = await this.userRepository.findOne(payload.id);
+        return user;
     }
 }
